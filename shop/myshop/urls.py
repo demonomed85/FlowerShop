c25aefaaf791f1e86b16298f0bcd262ffd8881cb
demonomed85 <politeh@mail.ru>
2024-12-14 00:36:47 +0300

    Static
    Подключил статику, добавил html страницы, создал навигационное меню.

--- shop/myshop/urls.py
+++ shop/myshop/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('basket/', views.basket, name='basket'),
+    path('item/', views.item, name='item'),
+    path('logout/', views.logout, name='logout'),
+    path('order/', views.order, name='order'),
+    path('registration/', views.registration, name='registration'),
+    path('login/', views.login, name='login'),
 ]
--- shop/myshop/views.py
+++ shop/myshop/views.py
@@ -1,5 +1,24 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Цветочный магазин</h1>")
-# Create your views here.
+    return render(request, 'myshop/index.html')
+
+def basket(request):
+    return render(request, 'myshop/basket.html')
+
+def item(request):
+    return render(request, 'myshop/item.html')
+
+def logout(request):
+    return render(request, 'myshop/logout.html')
+
+def order(request):
+    return render(request, 'myshop/order.html')
+
+def registration(request):
+    return render(request, 'myshop/registration.html')
+
+def login(request):
+    return render(request, 'myshop/login.html')
+
