251849bde35842461c6ae898e04c837f8f45b67f
demonomed85 <politeh@mail.ru>
2025-02-01 20:13:54 +0300

    +1

--- shop/manage.py
+++ shop/manage.py
@@ -2,6 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import threading
+import asyncio
+from django.core.management import execute_from_command_line
+
+#from myshop.TG_bot.tg_manager_bot import start_bot
 
 
 def main():
@@ -15,8 +20,19 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
+
+#def run_bot():
+#    asyncio.run(start_bot())
+
+#    t = threading.Thread(target=run_bot)
+#    t.start()
+
+# Запуск бота
+# asyncio.run(start_bot())
+
+# Запуск Django
+    execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
